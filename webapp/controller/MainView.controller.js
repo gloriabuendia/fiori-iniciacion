ea796da8d412fed16bb6b84e7295bcf5bb61ff11
gloriabuendia <gloria.buendia@scl-consulting.com>
2021-07-13 15:31:01 +0000

    Fin de la App Fiori

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -1,10 +1,14 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
 ],
 	/**
 	 * @param {typeof sap.ui.core.mvc.Controller} Controller
+     * @param {typeof sap/ui/model/Filter} Filter
+     * @param {typeof sap/ui/model/FilterOperator} FilterOperator
 	 */
-    function (Controller) {
+    function (Controller, Filter, FilterOperator) {
         "use strict";
 
         return Controller.extend("logaligroup.myinvoices.controller.MainView", {
@@ -16,13 +20,29 @@ sap.ui.define([
             },
             
             onFilter: function (oEvent) {
-                
+                const oData = this.getView().getModel("selectionScreen").getData();
+                let filters = [];
+
+                if (oData.ShipName !== ""){
+                    filters.push(new Filter("ShipName", FilterOperator.Contains, oData.ShipName));
+                }
+                if (oData.ContryKey !== ""){
+                    filters.push(new Filter("Country", FilterOperator.EQ, oData.ContryKey));
+                }
+
+                const oList = this.getView().byId("invoicesList");
+                const oBinding = oList.getBinding("items");
+                oBinding.filter(filters);
             },
 
             onClearFilters: function () {
                 const oModelSelScreen = this.getView().getModel("selectionScreen");
                 oModelSelScreen.setProperty("/ShipName", "");
                 oModelSelScreen.setProperty("/ContryKey", "");
+
+                const oList = this.getView().byId("invoicesList");
+                const oBinding = oList.getBinding("items");
+                oBinding.filter([]);
 
             }
         });
